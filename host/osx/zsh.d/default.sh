c064928ab00184aca8e5d64af7bc49f7727c7f2d
Andrew Goudzwaard <adgoudz@gmail.com>
2018-08-05 16:43:07 -0400

    Improve path for python/pyenv/etc. and bring theme up-to-date with work theme

--- host/osx/zsh.d/default.sh
+++ host/osx/zsh.d/default.sh
@@ -1,15 +1,24 @@
 
 BASE=$(cd $(dirname ${BASH_SOURCE-$0}) && pwd -P)/../../..
 
-#-----------
-# Shortcuts
-#-----------
+#-------------
+# Directories
+#-------------
+
+# These are sourced before anything else because they
+# are referenced in the shared rc file.
 
 export SANDBOX=$HOME/Sandbox
 export BIN=$SANDBOX/bin
 export DOTFILES=$SANDBOX/dotfiles
 export INSTALLS=$SANDBOX/installs
 export REPOS=$SANDBOX/repos
+
+#--------
+# Shared
+#--------
+
+source $BASE/host/shared/zsh.d/default.sh
 
 #---------------
 # Configuration
@@ -21,31 +30,27 @@ export CLICOLOR=1
 # Development
 #-------------
 
-export PYTHON=/usr/local/bin/python2
-export PYTHON3=/usr/local/bin/python3
-
-export PYTHON_USER_BASE=$($PYTHON -m site --user-base)
-export PYTHON3_USER_BASE=$($PYTHON3 -m site --user-base)
-
-export PYENV_ROOT=$HOME/.local/pyenv
-export WORKON_HOME=$HOME/.local/venvs
-
-export NODE_ENV=development
+# pyenv virtualenv might not be installed under $PYENV_ROOT
+export PYENV_VIRTUALENV_INSTALL_PREFIX=$( \
+    export VIRTUALENV_INIT_DIRNAME=$(dirname $(which pyenv-virtualenv-init)); \
+    export VIRTUALENV_INIT_LINK=$(readlink $(which pyenv-virtualenv-init)); \
+    cd $(dirname "$VIRTUALENV_INIT_DIRNAME/$(dirname $VIRTUALENV_INIT_LINK)") \
+        && pwd -P \
+)
 
 #------
 # Path
 #------
 
+# Front of path (added in reverse)
+pathmunge $PYTHON_USER_BASE/bin   # pip2 installs here
+pathmunge $PYTHON3_USER_BASE/bin  # pip3 installs here
+pathmunge $HOME/.local/bin  # pipsi installs here
+pathmunge $PYENV_ROOT/bin  # pyenv
+
 eval "$(/usr/local/bin/pyenv init -)"
-
-# Front of path (in reverse)
-pathmunge $PYTHON_USER_BASE/bin
-pathmunge $PYTHON3_USER_BASE/bin
+eval "$(/usr/local/bin/pyenv virtualenv-init -)"
 pathmunge $PYENV_ROOT/shims
-pathmunge $HOME/.local/bin
+pathmunge $PYENV_VIRTUALENV_INSTALL_PREFIX/shims
 
-#--------
-# Shared
-#--------
 
-source $BASE/host/shared/zsh.d/default.sh
--- oh-my-zsh/andrew.zsh-theme
+++ oh-my-zsh/andrew.zsh-theme
@@ -1,9 +1,12 @@
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[green]%}|%b%{$fg[green]%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}%b %{$fg[cyan]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX=""
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[green]%}|%b %{$fg[red]%}✗ "
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}|%b "
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}✗ "
+ZSH_THEME_GIT_PROMPT_CLEAN="%b "
 
-PROMPT='%{$fg[blue]%}%3~ $(git_prompt_info)%{$reset_color%}'
+ZSH_THEME_VIRTUALENV_PREFIX="%{$fg_bold[cyan]%}ⓔ%b %{$fg_bold[magenta]%}"
+ZSH_THEME_VIRTUALENV_SUFFIX="%b "
+
+PROMPT='%{$fg[blue]%}%3~ $(virtualenv_prompt_info)$(git_prompt_info)%{$reset_color%}'
 
 export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
